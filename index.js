0ce3405b114e7f9f48ea1ffe6b513885e50f974d
Gaurav Prajapati <115924293+gauravprajapati23@users.noreply.github.com>
2025-03-02 22:04:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -32,6 +32,29 @@ client.connect(err => {
     socket.on('disconnect', () => {
       console.log('Client disconnected');
     });
+  });
+
+  app.get('/api/data', async (req, res) => {
+    const data = await db.collection('testCollection').find().toArray();
+    res.json(data);
+  });
+
+  app.post('/api/data', express.json(), async (req, res) => {
+    const result = await db.collection('testCollection').insertOne(req.body);
+    res.json(result.ops[0]);
+  });
+
+  app.delete('/api/data/:id', async (req, res) => {
+    const result = await db.collection('testCollection').deleteOne({ _id: ObjectId(req.params.id) });
+    res.json({ deleted: result.deletedCount });
+  });
+
+  app.put('/api/data/:id', express.json(), async (req, res) => {
+    const result = await db.collection('testCollection').updateOne(
+      { _id: ObjectId(req.params.id) },
+      { $set: req.body }
+    );
+    res.json({ modified: result.modifiedCount });
   });
 
   server.listen(3000, () => {
